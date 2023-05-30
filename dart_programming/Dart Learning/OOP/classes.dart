0af856f3c94b5c4568fd9a2dcf27a8c61533e030
Exalter104 <muhammadsherazkhan104@gmail.com>
2023-05-31 00:41:58 +0500

    OOP Initiliazation :Constructor with excercise are done

---
+++ dart_programming/Dart Learning/OOP/classes.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: non_constant_identifier_names
+
+/*
+
+------------------------------------->CLASS IN DART<----------------------------
+*DEFINITION OF CLASS :
+--------------------
+"
+In OOP, a class is a blueprint for creating objects.A class defines the (properties) 
+and (methods) that an object will have."
+*For Example: 
+------------
+A class called "Dog" might have properties like "breed, color" and methods like
+"bark, run.".
+
+*SYNTEX:
+------
+class ClassName {
+  *Properties or data member or feilds
+  *Methods or function
+}
+
+*SYNTEX DESCRIPTIONS:
+-------------------
+1:ClassName is the name of the class and must start with capital letter.
+2:Body of the class consists of properties and functions.
+3:(*Properties): are used to store the data. It is also known as fields or attributes.
+4:(*Functions ): are used to perform the operations. It is also known as methods.
+
+*/
+
+//*PRACTICAL WORK
+
+//*Example 1: Declaring A Class In Dart
+
+//*CLASS -1
+
+class Exarth {
+  String? CTO;
+  String? teamLead;
+  int? noOfMembers;
+
+//*METHOD -1
+
+  void display() {
+    print("The CTO of Exarth is $CTO");
+    print("The TeamLead of Exarth is $teamLead");
+    print("The members of Exarth is $noOfMembers");
+  }
+}
+
+//*Example 2: Declaring Area Class In Dart
+
+//*CLASS -2
+
+class Area {
+  int? length;
+  int? width;
+
+//*METHOD -2
+
+   display() {
+    return length! * width!;
+  }
+}
---
+++ dart_programming/Dart Learning/OOP/objects.dart
@@ -0,0 +1,84 @@
+/*
+
+------------------------------------->OBJECTS IN DART<----------------------------
+*DEFINITION OF OBJECTS :
+--------------------
+"
+In OOP, Objects are created from templates called classes. An object is made up 
+of properties(variables) and methods(functions). An object is an instance of a class.
+
+*For Example: 
+------------
+For example, if you have a class called Bicycle, then you can create an object 
+of the class called bicycle.
+
+*SYNTEX:
+-------
+*ClassName objectName = ClassName();
+void main(){
+Bike (bike <=== OBJECT) =Bike();
+bike.color,
+bike.brake();
+}
+
+
+*NOTE DESCRIPTIONS:
+-------------------
+To create an object, you must create a class first.
+
+*/
+//*Practical WORK
+
+//*Example 1:Declaring An Object In Dart
+
+//*CLASS -1
+class Bycycle {
+  String? color;
+  int? size;
+  int? currentSpeed;
+
+//*METHOD -1
+  void changeGear(int newValue) {
+    currentSpeed = newValue;
+  }
+
+//*METHOD -2
+  void display() {
+    print("The Color of Car is : $color");
+    print("The Size of Car is : $size");
+    print("The Speed of Car is : $currentSpeed");
+  }
+}
+
+//*MAIN METHOD
+void main() {
+  // Bycycle object = Bycycle();
+  // object.color = "Red";
+  // object.size = 23;
+  // object.currentSpeed;
+  // object.changeGear(34);
+  // object.display();
+
+  Camera object1 = Camera();
+  object1.displayCamera();
+  Camera object2 = Camera();
+  object2.displayCamera();
+}
+//*CHALLENG :
+//Create a class Camera with properties: name, color, megapixel. Create a method
+// called display which prints out the values of the three properties. Create two
+// objects of the class Camera and call the method display.
+//*CLASS -2
+
+class Camera {
+  String? color="red";
+  String? name="Canon";
+  int? megaPixels=50;
+
+//*METHOD -2
+  void displayCamera() {
+    print("The Color of Camera is : $color");
+    print("The Name of Camera is : $name");
+    print("The MegaPixels of Camera is : $megaPixels");
+  }
+}
